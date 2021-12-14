5142becfdd2620b96dc0682209e2611236cefedf
BethanyFolino <bethany.folino@gmail.com>
2021-12-14 15:50:39 -0800

    Completed section 4 part 49 More on Methods and a Challenge - using the methods from Methods in Java, got challenged to make methods for displaying high score position and calculating high score position and adding those to the main method, applied fix to logic hole mentioned in video

--- src/com/bethany/Methods.java
+++ src/com/bethany/Methods.java
@@ -7,14 +7,53 @@ public class Methods {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Bethany", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Andrew", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Ann", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Joe", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Josh", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Alex", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Jack", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition +
+                " on the high score table!");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if(playerScore >= 1000){
+            return 1;
+        } else if(playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -22,11 +61,9 @@ public class Methods {
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
         }
-
     }
 }
