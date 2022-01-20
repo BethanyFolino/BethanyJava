fe5e910b3cd95e6630a5f3d87157383ef9905125
BethanyFolino <bethany.folino@gmail.com>
2022-01-20 11:37:43 -0800

    Completed Section 4 Coding Exercise 10: Equality Printer

---
+++ src/com/bethany/EqualityPrinter.java
@@ -0,0 +1,22 @@
+package com.bethany;
+
+public class EqualityPrinter {
+    public static void main(String[] args) {
+        printEqual(-1,1,2);
+        printEqual(1,1,1);
+        printEqual(1,2,3);
+        printEqual(1,2,1);
+    }
+
+    public static void printEqual(int one, int two, int three) {
+        if (one < 0 || two < 0 || three < 0) {
+            System.out.println("Invalid Value");
+        } else if (one == two && one == three) {
+            System.out.println("All numbers are equal");
+        } else if (one == two || two == three || one == three) {
+            System.out.println("Neither all are equal or different");
+        } else {
+            System.out.println("All numbers are different");
+        }
+    }
+}
